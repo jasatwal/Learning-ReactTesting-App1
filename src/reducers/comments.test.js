1bc464fb9c3b1ccd806efe83f6665fd510f6bacd
Jaspreet Atwal <jatwal@capaxglobal.com>
2018-10-09 21:55:03 +0100

    Added test from comments reducer.

---
+++ src/reducers/comments.test.js
@@ -0,0 +1,12 @@
+import commentsReducer from './comments';
+import { SAVE_COMMENT } from '../actions/types';
+
+it('handles actions for SAVE_COMMENT', () => {
+  const action = {
+    type: 'SAVE_COMMENT',
+    payload: 'new comment'
+  };
+
+  const newState = commentsReducer([], action);
+  expect(newState).toEqual(['new comment']);
+});
