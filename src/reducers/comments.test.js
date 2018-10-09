79c2fb485b61b4d012d41ee8a013fa1c6415fb48
Jaspreet Atwal <jatwal@capaxglobal.com>
2018-10-09 22:02:33 +0100

    Added test for saveComment action creator.

--- src/reducers/comments.test.js
+++ src/reducers/comments.test.js
@@ -3,10 +3,15 @@ import { SAVE_COMMENT } from '../actions/types';
 
 it('handles actions for SAVE_COMMENT', () => {
   const action = {
-    type: 'SAVE_COMMENT',
+    type: SAVE_COMMENT,
     payload: 'new comment'
   };
 
   const newState = commentsReducer([], action);
   expect(newState).toEqual(['new comment']);
+});
+
+it('handles actions with unknow type', () => {
+  const newState = commentsReducer([], {});
+  expect(newState).toEqual([]);
 });
